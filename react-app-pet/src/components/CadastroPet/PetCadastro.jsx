e65c34f035eb9296267c1471d130a0f909030d62
VitorRT <barberino.italia@gmail.com>
2022-09-25 21:17:24 -0300

    Co-authored-by: Bruno Bezerra Brasil <brunobrasilll@users.noreply.github.com> Co-authored-by: PedroLu1z <PedroLu1z@users.noreply.github.com>

--- react-app-pet/src/components/CadastroPet/PetCadastro.jsx
+++ react-app-pet/src/components/CadastroPet/PetCadastro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PetBg from './img/pet-bg3.jpg'
 import { ButtonSubmit, FormPet } from '../../style/styled'
 import './cadastro.css'
 
@@ -6,61 +7,68 @@ function CadastroPet() {
   return (
     <div className='form-cadastro-pet'>
       <FormPet action="" method="post">
-        <h3>Cadastro</h3>
+        <h3>Cadastre seu Pet</h3>
 
         {/*Nome pet*/}
         <div>
           <div></div>
-          <input type="text" placeholder='Digite o nome do Pet' />
+          <input type="text" placeholder='Digite o nome do Pet' required/>
+          <label>Nome Pet</label>
         </div>
 
 
         {/*Idade Pet*/}
         <div>
           <div></div>
-          <input type="number" placeholder='Digite a Idade do Pet' />
+          <input type="number" placeholder='Digite a Idade do Pet' required/>
+          <label>Idade Pet</label>
         </div>
 
 
         {/*Raça*/}
         <div>
           <div></div>
-          <input type="text" placeholder='Digite a Raça do Pet'/>
+          <input type="text" placeholder='Digite a Raça do Pet' required/>
+          <label>Raça Pet</label>
         </div>
 
 
         {/*Tamanho*/}
         <div>
           <div></div>
-          <input type="number" placeholder='Digite o Tamanho do Pet' />
+          <input type="number" placeholder='Digite o Tamanho do Pet' required/>
+          <label>Tamanho Pet</label>
         </div>
 
 
         {/*Nome do Dono*/}
         <div>
           <div></div>
-          <input type="text" placeholder='Digite o Nome do(a) Dono(a)' />
+          <input type="text" placeholder='Digite o Nome do(a) Dono(a)' required/>
+          <label>Nome Dono</label>
         </div>
 
 
         {/*Telefone do Dono*/}
         <div>
           <div></div>
-          <input type="text" placeholder='Digite o Nome do(a) Dono(a)'/>
+          <input type="text" placeholder='Digite o Nome do(a) Dono(a)' required/>
+          <label>Telefone Dono</label>
         </div>
 
 
         {/*Imagem do Pet*/}
         <div>
           <div></div>
-          <input type="text" placeholder='Coloque uma Imagem de Seu Pet' />
+          <input type="file" required/>
+          <label>Imagem Pet</label>
         </div>
 
 
         {/*Observações*/}
         <div>
           <div></div>
-          <textarea type="text" placeholder='Digite as Observações'/>
+          <textarea type="text" placeholder='Digite as Observações' cols='70' rows='3'/>
         </div>
         
 
@@ -70,8 +78,10 @@ function CadastroPet() {
       </FormPet>
 
       <div className="text-pet">
+        <img src={PetBg}  />
         <div>
-          <h2>A atenção que seu pet merece!</h2>            
+          <h2>A atenção que seu pet merece!</h2>
+          <h5>venha fazer parte da familia Pet-Care!</h5>            
         </div>
       </div>
 
--- react-app-pet/src/style/styled.js
+++ react-app-pet/src/style/styled.js
@@ -3,12 +3,22 @@ import styled from 'styled-components'
 export const ButtonSubmit = styled.a `
     background-color: #fff;
     border: 2px solid #ddd;
+    font-family: 'Roboto Condensed', sans-serif;
     color: #000;
     padding: 5px;
+    width: 100%;
+    height: 53px;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+    text-align:center;
+    font-weight: bold;
+    font-size: 20px;
+    border-radius: 4px;
     &:hover{
         cursor: pointer;
-        transition: all 0.4s;
-        background-color: #151b26;
+        transition: all ease-in-out 150ms;
+        background-color: blueviolet;
         color: #fff;
     }
 `
@@ -20,18 +30,74 @@ export const FormPet = styled.form`
     background-color: #fff;
     padding: 40px;
 
-
-    input {
-        border: unset;
-        outline: none;
-        width: 100%;
-        margin: 6px;
-        border-bottom: 2px solid #ddd;
+    h3{
+        font-family: 'Roboto Condensed', sans-serif;
+        font-size: 1.5rem;
+        margin-bottom: 1.2rem;
+        position: relative;
+        ::before {
+            content: '';
+            position: absolute;
+            width: 32%;
+            height: 2px;
+            background-color: blueviolet;
+            bottom: 0;
+        }
     }
+    div {
+        margin: 2rem 0px;
+        input {
+            letter-spacing: 1.8px;
+            font-size: 14.5px;
+            font-family: 'Roboto Condensed', sans-serif;
+            border: unset;
+            outline: none;
+            width: 100%;
+            margin-bottom: 1rem;
+            position: relative;
+            border-bottom: 2px solid #ddd;
+            outline: 0;
+            &::after {
+                content: '';
+                position: absolute;
+                width: 100%;
+                height: 2px;
+                background-color: blueviolet;
+                bottom: 0;
+            }
+    
+            &:focus, &:valid {
+                border-bottom: 2px solid blueviolet;
+            }
 
-    textarea {
-        outline:none;   
-        border-bottom: 2px solid #ddd;
+            &:focus ~ label, &:valid ~ label {
+                font-size: 12px;
+                transform: translateY(-24px) ;
+                transition: all 100ms ease-in-out;
+            }
+    
+            &::placeholder {
+                
+                opacity: 0;
+            }
+    
+        }
+    
+        label {
+            position: absolute;
+            font-family: 'Roboto Condensed', sans-serif;
+            left: 0;
+            margin: 0px 40px;
+            opacity: 50%;
+            transition: all 100ms ease-in-out;
+        }
+    
+       
+    
+        textarea {
+            padding: 5px;
+            resize:none;
+        }
     }
 `
 
